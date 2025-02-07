116a8caf6a1ea0e2c958e3429bd504e8ec297172
kwisener01 <158852969+kwisener01@users.noreply.github.com>
2025-02-06 20:26:44 -0500

    Update PMapp.py

--- PMapp.py
+++ PMapp.py
@@ -1,5 +1,5 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 import pandas as pd
 
 # Authenticate with Google Sheets
